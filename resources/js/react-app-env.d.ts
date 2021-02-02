78aa2596dbc78894fee9fd959d8b66e101365745
Sanjeev Kumar <upworksanjeev@gmail.com>
2021-02-02 16:27:42 +0530

    added libraries of correct versions

--- resources/js/react-app-env.d.ts
+++ resources/js/react-app-env.d.ts
@@ -4,3 +4,5 @@ declare module 'enzyme';
 declare module 'history';
 declare module 'd3-timer';
 declare module 'enzyme-adapter-react-16';
+declare module 'react-dom';
+declare module 'react-dom/test-utils';
